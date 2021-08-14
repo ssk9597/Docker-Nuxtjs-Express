d1e45175ccf5b3e51957e88734aad2f0956a48eb
sasaki <skyo.97.admj@gmail.com>
2021-08-14 15:39:58 +0900

    git init

--- api/app.js
+++ api/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
-// HTTPリクエストを受け取る部分
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// routing_file
+const indexRouter = require('./routes/index');
+
+// routing
+app.use('/', indexRouter);
 
 // サーバーを起動する部分
 app.listen(18080, function () {
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// controller_file
+const indexController = require('../controller/IndexController');
+
+// routing
+router.get('/', indexController.index);
 
 module.exports = router;
